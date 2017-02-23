079c279bdfac9b610e51c78790daed2b26d7928e
Vasily Melenchuk <bzzzil@gmail.com>
2017-02-23 13:40:37 +0300

    new: conan, lib

---
+++ hello/include/hello.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_HELLO_H
+#define HELLO_HELLO_H
+
+#include <ostream>
+
+namespace hello {
+		std::ostream& greet(std::ostream&);
+}
+#endif
+
---
+++ hello/src/hello.cpp
@@ -0,0 +1,7 @@
+#include <hello.h>
+
+std::ostream& hello::greet(std::ostream& stream) 
+{
+		return stream << "Hello, world";
+}
+
